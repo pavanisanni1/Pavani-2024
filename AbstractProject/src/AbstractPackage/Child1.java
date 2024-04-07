d1f569825d8fa7291510f5c894b0358dcb8b2d6c
Pavanisanni1 <pavanisanni1.gmail.com>
2024-04-07 10:53:54 -0500

    second commit

---
+++ AbstractProject/src/AbstractPackage/Child1.java
@@ -0,0 +1,14 @@
+package AbstractPackage;
+
+public class Child1 extends Parent1{
+
+	/*public void Print(){
+		System.out.println("in child1 class");
+	}*/
+	public void sum() {
+		System.out.println("in sum method");
+	}
+	
+	
+
+}
---
+++ AbstractProject/src/AbstractPackage/Parent1.java
@@ -0,0 +1,11 @@
+package AbstractPackage;
+
+abstract class Parent1 {
+
+	public abstract void sum();
+	public void Print() {
+		System.out.println("in parent1 class");
+	}
+
+	
+}
